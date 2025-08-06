91600d1821202872bb49cea70baadc38dc8e5b0e
Garima Tiwari <garimatiwari1803@gmail.com>
2025-08-06 23:32:06 +0530

    Add react-router-dom dependency and refactor App component structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=' bg-gradient-to-t from-[#121313] to-[#19024f] flex flex-col items-center justify-center min-h-screen w-full'>
+      <h1 className='text-white text-7xl md:6xl font-extrabold'>New in the game - Not in the Grind</h1>
+      
     </div>
   );
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+
+export default function Navbar() {
+    return (
+        <div className="absolute top-4 left-1/2 transform -translate-x-1/2 flex flex-col justify-center items-center bg-gray-200 rounded-3xl w-[500px] p-4">
+        <nav className=" " >     
+            
+            <div className="flex justify-center gap-5" >     
+                <a href="/">Home</a>
+                <a href="/about">About</a>  
+                <a href="/projects">Projects</a>
+                <a href="/contact">Contact</a>
+            </div>
+        </nav>  
+        </div>
+    );
+}
