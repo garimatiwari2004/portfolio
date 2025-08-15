03a4024ae0cf6ad9b39cd865aa90f027d445592e
Garima Tiwari <garimatiwari1803@gmail.com>
2025-08-15 22:22:48 +0530

    Remove responsive text size classes from Hero component heading for simplification

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className=" herodiv flex flex-col items-center justify-center min-h-screen w-full ">
       <div className=" text-center ">
-        <h1 className="text-white text-4xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold  tag">
+        <h1 className="text-white text-4xl font-extrabold  tag">
           New in the game,
           <br/>
            Not in the Grind
