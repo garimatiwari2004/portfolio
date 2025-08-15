e370c742a8063b97294e35d53a1e75a4ece29720
Garima Tiwari <garimatiwari1803@gmail.com>
2025-08-15 22:25:30 +0530

    Update Hero component heading to improve text visibility on medium devices

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className=" herodiv flex flex-col items-center justify-center min-h-screen w-full ">
       <div className=" text-center ">
-        <h1 className="text-white text-4xl font-extrabold  tag">
+        <h1 className="text-white md:text-5xl text-4xl font-extrabold  tag">
           New in the game,
           <br/>
            Not in the Grind
